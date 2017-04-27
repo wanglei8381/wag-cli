a1e55d9c64a879d8d94f869dbdfb19af56daeb58
wanglei8381 <wanglei8381@163.com>
2017-04-27 16:42:13 +0800

    chore: 调整测试的入口

--- template/test/unit/index.js
+++ template/test/unit/index.js
@@ -5,6 +5,3 @@ Vue.config.productionTip = false
 // 测试用例
 const testsContext = require.context('./specs', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
-
-// 源代码
-require('pages/home/src/App.vue')
