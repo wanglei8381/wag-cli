032154ad0c27ef194700e80dbe205a427939c8a2
wanglei8381 <wanglei8381@163.com>
2017-05-18 19:21:36 +0800

    修复测试覆盖率无效

--- config/webpack.config.test.js
+++ config/webpack.config.test.js
@@ -1,7 +1,15 @@
 var devWebpackConfig = require("./webpack.config.dev");
+var webpack = require("webpack");
 
 var webpackConfig = Object.assign({}, devWebpackConfig, {
-  devtool: '#inline-source-map'
+  devtool: '#inline-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"testing"'
+      }
+    })
+  ]
 })
 
 delete webpackConfig.entry
--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,22 @@
 process.env.NODE_ENV = 'test';
-process.env.BABEL_ENV = 'test';
-let spawn = require('cross-spawn');
+// let spawn = require('cross-spawn');
+let crossEnv = require('cross-env');
 let path = require('path');
 let args = process.argv.slice(2);
 if (!args.length) {
   args = ['--single-run']
 }
-spawn.sync(
+
+crossEnv([
+  'BABEL_ENV=test',
   'karma',
-  ['start', path.resolve(__dirname, '../config/karma.conf.js')].concat(args),
-  {stdio: 'inherit'}
-);
+  'start',
+  // 'test/unit/karma.conf.js'
+  path.resolve(__dirname, '../config/karma.conf.js')
+].concat(args))
+
+// spawn.sync(
+//   'karma',
+//   ['start', path.resolve(__dirname, '../config/karma.conf.js')].concat(args),
+//   { stdio: 'inherit', shell: true, env: { BABEL_ENV: 'test' } }
+// );
